71c023e645798257ffde8a8b768adfe9b66658ad
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-01 08:55:43 +0100

    added links section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Links from './components/Links/Links';
 import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <>
       <Profile />
+      <main>
+        <Links />
+      </main>
     </>
   );
 }

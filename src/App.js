9bd30789e439b62b68541b101617734c5decab26
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-01 15:42:32 +0100

    added username state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import Footer from './components/Footer/Footer';
 import Links from './components/Links/Links';
 import Profile from './components/Profile/Profile';
 import Socials from './components/Socials/Socials';
 
 function App() {
+  const [username, setUsername] = useState('devBayo');
+
   return (
     <>
-      <Profile />
+      <Profile username={username} />
       <main>
-        <Links />
+        <Links username={username} />
         <Socials />
       </main>
       <Footer />

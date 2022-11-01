53ef254bab38377237629f01326602149433e396
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-01 15:42:58 +0100

    added username

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -1,46 +1,5 @@
 import styles from './Links.module.css';
 
-const links = [
-  {
-    id: 'twitter',
-    href: 'https://twitter.com/devBayo',
-    content: 'Twitter Link',
-    title: '',
-  },
-  {
-    id: 'btn_zuri',
-    href: 'https://training.zuri.team/',
-    content: 'Zuri Team',
-    title: '',
-  },
-  {
-    id: 'books',
-    href: 'https://books.zuri.team/',
-    content: 'Zuri Books',
-    title: 'This is where you find books about design and coding',
-  },
-
-  {
-    id: 'book_python',
-    href: 'https://books.zuri.team/python-for-beginners?ref_id=<devBayo>',
-    content: 'Python Books',
-    title:
-      'Are you wondering how to get started with programming in Python? Here is a perfect chance for you.',
-  },
-  {
-    id: 'pitch',
-    href: 'https://background.zuri.team/',
-    content: 'Background Check for Coders',
-    title: "Here' the right place to get your background check done!",
-  },
-  {
-    id: 'book_design',
-    href: 'https://books.zuri.team/design-rules',
-    content: 'Design Books',
-    title: 'Check out the free design book offered by Zuri',
-  },
-];
-
 const LinkItem = props => {
   return (
     <li className={styles.link__item}>
@@ -57,7 +16,48 @@ const LinkItem = props => {
   );
 };
 
-const Links = () => {
+const Links = ({ username }) => {
+  const links = [
+    {
+      id: 'twitter',
+      href: `https://twitter.com/${username}`,
+      content: 'Twitter Link',
+      title: '',
+    },
+    {
+      id: 'btn_zuri',
+      href: 'https://training.zuri.team/',
+      content: 'Zuri Team',
+      title: '',
+    },
+    {
+      id: 'books',
+      href: 'https://books.zuri.team/',
+      content: 'Zuri Books',
+      title: 'This is where you find books about design and coding',
+    },
+
+    {
+      id: 'book_python',
+      href: `https://books.zuri.team/python-for-beginners?ref_id=${username}`,
+      content: 'Python Books',
+      title:
+        'Are you wondering how to get started with programming in Python? Here is a perfect chance for you.',
+    },
+    {
+      id: 'pitch',
+      href: 'https://background.zuri.team/',
+      content: 'Background Check for Coders',
+      title: "Here' the right place to get your background check done!",
+    },
+    {
+      id: 'book_design',
+      href: 'https://books.zuri.team/design-rules',
+      content: 'Design Books',
+      title: 'Check out the free design book offered by Zuri',
+    },
+  ];
+
   return (
     <ul className={styles.links}>
       {links.map(link => (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,12 @@
 import styles from './Profile.module.css';
 import profilePhoto from '../../assets/profile.jpeg';
 
-const Profile = () => {
+const Profile = ({ username }) => {
   return (
     <header id={styles.profile}>
       <img src={profilePhoto} id={styles.profile_img} alt="" />
       <h1 id={styles.twitter}>devBayo</h1>
-      <h2 id={styles.slack}>devBayo</h2>
+      <h2 id={styles.slack}>{username}</h2>
     </header>
   );
 };

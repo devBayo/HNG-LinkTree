c8c36368a0d98b848e29fd7c35dcaf8abfb2b2ec
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-01 09:45:42 +0100

    added footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,13 @@
-import styles from './Footer.module.css';
+import './Footer.module.css';
 import zuriLogo from '../../assets/Vector.svg';
-import FooterText from '../../assets/text.svg';
 import I4GLogo from '../../assets/I4G.svg';
 
 const Footer = () => {
   return (
     <footer>
-      <img src={zuriLogo} alt="" />
-      <img src={FooterText} alt="" />
-      <img src={I4GLogo} alt="" />
+      <img src={zuriLogo} alt="Zuri logo" />
+      <h2>HNG Internship 9 Frontend Task</h2>
+      <img src={I4GLogo} alt="I4G logo" />
     </footer>
   );
 };

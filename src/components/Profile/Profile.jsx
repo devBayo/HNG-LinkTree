3ae5709fd234e75871da5742d5b2c66bc54b8386
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-01 16:08:41 +0100

    added profile icon

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import styles from './Profile.module.css';
 import profilePhoto from '../../assets/profile.jpeg';
+import shareIcon from '../../assets/share.svg';
 
 const Profile = ({ username }) => {
   return (
     <header id={styles.profile}>
       <img src={profilePhoto} id={styles.profile_img} alt="" />
-      <h1 id={styles.twitter}>devBayo</h1>
+      <img src={shareIcon} id={styles.share_profile} alt="" />
+      <h1 id={styles.twitter}>{username}</h1>
       <h2 id={styles.slack}>{username}</h2>
     </header>
   );

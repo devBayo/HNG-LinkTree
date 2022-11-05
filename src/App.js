bcb6296e354aa62963b2bd25dd4c59be32804a22
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-05 06:23:57 +0100

    feat: added contact to page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Contact from './components/Contact/Contact';
 import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
   const [username, setUsername] = useState('');
